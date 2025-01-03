01ad60d6e3761aedb38a766a642ac976bd6ce0d5
antonioortega733 <antonioortega.alum@insestatut.cat>
2025-01-03 18:15:04 +0100

    c4.1

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,13 +3,22 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+        {
+          data.allFile.nodes.map(node =>(
+            <li key={node.name}>
+              {node.name}
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
 }
+
 export const query = graphql`
   query {
     allFile {
@@ -21,5 +30,4 @@ export const query = graphql`
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
-
 export default BlogPage
